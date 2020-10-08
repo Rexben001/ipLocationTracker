fc0a9d26fde37c21f746de8607c54000c0616180
Rexben <rexben.rb@gmail.com>
2020-10-08 13:32:49 +0100

    find based on search

--- js/index.js
+++ js/index.js
@@ -1,110 +1,63 @@
-// window.onload = () => {
-//   fetch(
-//     'https://api.ipgeolocation.io/ipgeo?apiKey=API_KEY'
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const div = document.getElementById('display');
-
-//       console.log(data);
-//       const items = [];
-//       for (let key in data) {
-//         console.log(key, data[key]);
-//         const p = document.createElement('p');
-//         const span = document.createElement('span');
-//         const span2 = document.createElement('span');
-//         const pp = document.createTextNode(`"${key}:" `);
-//         const pp2 = document.createTextNode(`${data[key]}`);
-//         span.appendChild(pp);
-//         span2.appendChild(pp2);
-//         p.appendChild(span);
-//         p.appendChild(span2);
-//         // const pp = `<p><span>${key}:</span> <span>${data[key]}</span></p>`;
-//         div.appendChild(p);
-//       }
-//     });
-// };
-
-const data = {
-  calling_code: '+234',
-  city: 'Lagos',
-  connection_type: 'wireless',
-  continent_code: 'AF',
-  continent_name: 'Africa',
-  country_capital: 'Abuja',
-  country_code2: 'NG',
-  country_code3: 'NGA',
-  country_flag: 'https://ipgeolocation.io/static/flags/ng_64.png',
-  country_name: 'Nigeria',
-  country_tld: '.ng',
-  currency: { code: 'NGN', name: 'Nigerian Naira', symbol: '₦' },
-  district: '',
-  geoname_id: '2332459',
-  ip: '197.210.47.239',
-  is_eu: false,
-  isp: 'Reserved 3GFW-MSP',
-  languages: 'en-NG,ha,yo,ig,ff',
-  latitude: '6.52438',
-  longitude: '3.37921',
-  organization: 'MTN NIGERIA Communication limited',
-  state_prov: 'Lagos',
-  time_zone: {
-    name: 'Africa/Lagos',
-    offset: 1,
-    current_time: '2020-10-07 22:51:39.715+0100',
-    current_time_unix: 1602107499.715,
-    is_dst: false,
-  },
-  zipcode: 'YABA',
+window.onload = () => {
+  fetchData();
 };
 
 const createItem = (data) => {
   const div = document.getElementById('display');
+
+  div.innerHTML = '';
+
+  const opening = document.createElement('span');
+  opening.className = 'opening';
+  const nodeOpening = document.createTextNode(`{`);
+  opening.appendChild(nodeOpening);
+  div.appendChild(opening);
+
   const items = [];
   for (let key in data) {
     const p = document.createElement('p');
     const span = document.createElement('span');
     let span2 = document.createElement('span');
-    const pp = document.createTextNode(
+    const pNode = document.createTextNode(
       key.includes('time_zone') || key.includes('currency')
         ? `"${key}": {`
         : `"${key}": `
     );
-    let pp2 = document.createTextNode(`${data[key]}`);
+    let pNode2 = document.createTextNode(`${data[key]}`);
     if (key.includes('time_zone') || key.includes('currency')) {
       const newData = data[key];
-      pp2 = document.createElement('p');
+      pNode2 = document.createElement('p');
       for (let key in newData) {
-        pp2.className = 'inner';
+        if (!newData[key]) continue;
+        pNode2.className = 'inner';
         const span = document.createElement('span');
         span.className = 'left';
         const span2 = document.createElement('span');
         span2.className = 'right';
-        const pp = document.createTextNode(`"${key}": `);
-        const pp3 = document.createTextNode(`${newData[key]}`);
+        const pNode = document.createTextNode(`"${key}": `);
+        const pNode3 = document.createTextNode(`${newData[key]}`);
         const br = document.createElement('br');
 
-        span.appendChild(pp);
-        span2.appendChild(pp3);
-        pp2.appendChild(span);
-        pp2.appendChild(span2);
-        pp2.appendChild(br);
-        // pp2.appendChild(p);
+        span.appendChild(pNode);
+        span2.appendChild(pNode3);
+        pNode2.appendChild(span);
+        pNode2.appendChild(span2);
+        pNode2.appendChild(br);
       }
       const closing = document.createElement('span');
       closing.className = 'closing';
       const node = document.createTextNode(`}`);
       closing.appendChild(node);
-      pp2.appendChild(closing);
+      pNode2.appendChild(closing);
     }
 
     if (key.includes('country_flag')) {
       span2 = document.createElement('img');
       span2.src = data[key];
     } else {
-      span2.appendChild(pp2);
+      span2.appendChild(pNode2);
     }
-    span.appendChild(pp);
+    span.appendChild(pNode);
     p.appendChild(span);
     p.appendChild(span2);
     div.appendChild(p);
@@ -116,7 +69,18 @@ const createItem = (data) => {
   div.appendChild(closing);
 };
 
-createItem(data);
+const fetchData = (query) => {
+  fetch(
+    query
+      ? `https://api.ipgeolocation.io/ipgeo?apiKey=API_KEY&ip=${query}`
+      : `https://api.ipgeolocation.io/ipgeo?apiKey=API_KEY&`
+  )
+    .then((response) => response.json())
+    .then((data) => createItem(data))
+    .catch((err) => err);
+};
 
-const input = document.getElementById('input').value;
-console.log('input>>>>', input);
+document.getElementById('search').addEventListener('click', () => {
+  const input = document.getElementById('inputValue').value;
+  fetchData(input);
+});
